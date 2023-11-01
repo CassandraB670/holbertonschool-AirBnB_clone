801df7e88f24a24bebcf0462c246257aa617cfdf
CassandraB670 <cassandra.boudier@gmail.com>
2023-11-01 22:24:26 +0000

    update base_model with storage variable

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 """Write the first class Base"""
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
@@ -26,6 +27,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            storage.new(self)
 
     def __str__(self):
         """print representation str of the BaseModel Class
@@ -40,6 +42,7 @@ class BaseModel:
         updated_at : with the current datetime
         """
         self.updated_at = datetime.now()
+        storage.save(self)
 
     def to_dict(self):
         """returns a dictionary
